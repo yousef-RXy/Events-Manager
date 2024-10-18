aea560c6e9501da0dc1416dbe0aad35df71390f7
Yousef Na <yousefnasr0541@gmail.com>
2024-10-18 22:23:34 +0300

    fix add event navigation and invalidateQueries

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { useMutation } from '@tanstack/react-query';
-import { createNewEvent } from '../../util/http.js';
+import { createNewEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
@@ -11,6 +11,10 @@ export default function NewEvent() {
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('..');
+    },
   });
 
   function handleSubmit(formData) {
