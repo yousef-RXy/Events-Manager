762faef8e1ca3ddbc427ce0b898c69a23a68d596
Yousef Na <yousefnasr0541@gmail.com>
2024-10-19 10:54:51 +0300

    add delete event

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,54 +1,70 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvent } from '../../util/http.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events', id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
+  const {
+    mutate,
+    // isPending: isDeletePending,
+    // isError: isDeleteError,
+    // error,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      console.log('hi');
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none',
+      });
+      navigate('..');
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
+
   return (
     <>
-      {isError && (
-        <ErrorBlock
-          title="An error occurred"
-          className="center"
-          message={error.info?.message || 'Failed to fetch event details'}
-        />
-      )}
-      {isPending && (
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            minHeight: 'fit-content',
-            height: '70dvh',
-          }}
-        >
-          <LoadingIndicator />
-        </div>
-      )}
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">
+        {isError && (
+          <div id="event-details-content" className="center">
+            <ErrorBlock
+              title="An error occurred"
+              className="center"
+              message={error.info?.message || 'Failed to fetch event details'}
+            />
+          </div>
+        )}
+        {isPending && (
+          <div id="event-details-content" className="center">
+            <LoadingIndicator />
+          </div>
+        )}
 
-      {data && (
-        <>
-          <Outlet />
-          <Header>
-            <Link to="/events" className="nav-item">
-              View all Events
-            </Link>
-          </Header>
-          <article id="event-details">
+        {data && (
+          <>
             <header>
               <h1>{data.title}</h1>
               <nav>
-                <button>Delete</button>
+                <button onClick={handleDelete}>Delete</button>
                 <Link to="edit">Edit</Link>
               </nav>
             </header>
@@ -60,16 +76,20 @@ export default function EventDetails() {
               <div id="event-details-info">
                 <div>
                   <p id="event-details-location">{data.location}</p>
-                  <time
-                    dateTime={`Todo-DateT$Todo-Time`}
-                  >{`${data.date}:${data.time}`}</time>
+                  <time dateTime={`Todo-DateT$Todo-Time`}>{`${new Date(
+                    data.date
+                  ).toLocaleDateString('en-US', {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric',
+                  })} @ ${data.time}`}</time>
                 </div>
                 <p id="event-details-description">{data.description}</p>
               </div>
             </div>
-          </article>
-        </>
-      )}
+          </>
+        )}
+      </article>
     </>
   );
 }
